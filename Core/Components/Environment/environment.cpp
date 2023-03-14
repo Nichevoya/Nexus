70a77702f13212a1e14becd26c52c6ceccdd353b
Shyonome <isaak.nicolas@epitech.eu>
2023-03-14 19:36:04 +0100

    [+] environment component

---
+++ Core/Components/Environment/environment.cpp
@@ -0,0 +1,82 @@
+#include "environment.hpp"
+
+using namespace nexus::core::components;
+
+environment::environment(const int ac, const char **av) : _ac(ac), _av(av)
+{
+    logs("Terminal component start");
+    initialization();
+}
+
+environment::~environment() { logs("Terminal component stop"); }
+
+void environment::initialization(void)
+{
+    while (this->_is_active && (get_input() != "no" || get_input() != "yes")) {
+        log(terminal_log_type::message, "Do you wish to use the Nexus environment ? (yes/no)");
+        set_log_type(terminal_log_type::nexus);
+        input();
+        if (get_input() == "no") {
+            this->_is_active = false;
+        } else if (get_input() == "yes") {
+            break;
+        }
+    }
+}
+
+void environment::format(void)
+{
+    switch (get_log_type()) {
+        case terminal_log_type::nexus: std::cout << "[nexus]: "; break;
+        case terminal_log_type::message: std::cout << "[message]: "; break;
+        case terminal_log_type::error: std::cout << "[error]: "; break;
+        default: break;
+    }
+}
+
+void environment::log(const terminal_log_type &log_type, const std::string &message)
+{
+    set_log_type(log_type);
+    std::cout << message << std::endl;
+}
+
+void environment::read(void)
+{
+    while (_app->is_active() && this->_is_active) {
+        format(); input();
+        set_command_type_from_input();
+        set_log_type(terminal_log_type::nexus);
+    }
+}
+
+void environment::input(void)
+{
+    std::getline(std::cin, _input);
+}
+
+const std::string &environment::input(const std::string &message)
+{
+    std::cout << "[message]: " << message << std::endl;
+    std::getline(std::cin, _input);
+    return _input;
+}
+
+void environment::set_command_type_from_input(void)
+{
+    if (_input == "h" || _input == "help") _command.set_type(command_type::help);
+    else if (_input == "q" || _input == "quit") _command.set_type(command_type::quit);
+    else if (_input == "load") _command.set_type(command_type::load);
+    else _command.set_type(command_type::none);
+    action();
+}
+
+void environment::action(void)
+{
+    switch (_command.get_type()) {
+        case command_type::none: break;
+        case command_type::help: _command.help(); break;
+        case command_type::quit: _command.quit(); break;
+        case command_type::load: _command.load(); break;
+        default: break;
+    }
+}
---
+++ Core/Components/Environment/environment.hpp
@@ -0,0 +1,66 @@
+#ifndef TERMINAL
+#define TERMINAL
+
+/* Source */
+#include "ressource.hpp"
+#include "command.hpp"
+
+enum class terminal_log_type {
+    none,
+    nexus,
+    message,
+    error,
+};
+
+namespace nexus {
+
+    namespace core {
+
+        namespace components {
+
+            class environment {
+                public:
+                    explicit environment(const int ac, const char **av);
+                    ~environment();
+
+                    void set_log_type(const terminal_log_type &log_type) { _log_type = log_type; format(); }
+
+                    const std::string &get_input(void) const { return _input; }
+                    const terminal_log_type &get_log_type(void) const { return _log_type; }
+                    
+                    void initialization(void);
+
+                    void format(void);
+                    void log(const terminal_log_type &log_type, const std::string &message);
+                    
+                    void read(void);
+                    
+                    void input(void);
+                    const std::string &input(const std::string &message);
+                    
+                    void set_command_type_from_input(void);
+                    void action(void);
+
+                protected:
+                private:
+                    bool _is_active = true;
+
+                    const int _ac;
+                    const char **_av;
+                    
+                    std::string _input = "";
+                    terminal_log_type _log_type = terminal_log_type::none;
+
+                    command _command = command_type::none;
+                    
+                    app::integrity::status *_app = app::integrity::status::get();
+            };
+            
+        } // components
+        
+    } // core
+
+} // nexus
+
+
+#endif /* TERMINAL */
