3ee47afcb333fbc07f788ad339ec719333f5912a
Shyonome <isaak.nicolas@epitech.eu>
2023-03-14 19:25:19 +0100

    [+] logic interface

---
+++ Core/Interface/logic.hpp
@@ -0,0 +1,30 @@
+#ifndef LOGIC
+#define LOGIC
+
+/* Source */
+#include "ressource.hpp"
+
+namespace nexus {
+    
+    namespace core {
+        
+        namespace interface {
+
+            namespace logic {
+
+                class game {
+                    public:
+                        virtual ~game() = default;
+                    protected:
+                    private:
+                };
+                
+            } // logic
+                
+        } // interface
+
+    } // core
+
+} // nexus
+
+#endif /* LOGIC */
