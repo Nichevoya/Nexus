188b23fc854969149370bd04d580ecc06c1a43b8
Shyonome <isaak.nicolas@epitech.eu>
2023-03-19 13:01:19 +0100

    [+] logs at construction and destruction

--- Module/Game/Hunter/hunter.cpp
+++ Module/Game/Hunter/hunter.cpp
@@ -4,6 +4,6 @@ using namespace nexus::module::logic;
 
 extern "C" const std::unique_ptr<hunter> entry_point(void) { return std::make_unique<hunter>(); }
 
-hunter::hunter() {}
+hunter::hunter() { logs("Hunter start"); }
 
-hunter::~hunter() {}
+hunter::~hunter() { logs("Hunter stop"); }
