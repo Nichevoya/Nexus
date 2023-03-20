95833fe32e532eda60d88c6f031f18429d211ce3
Shyonome <isaak.nicolas@epitech.eu>
2023-03-20 13:04:01 +0100

    [+] object as unique instance, list, load, remove and clear implementation

--- Core/Components/Plugin Loader/plugin.cpp
+++ Core/Components/Plugin Loader/plugin.cpp
@@ -2,18 +2,89 @@
 
 using namespace nexus::core::components;
 
-plugin::plugin() { logs("Plugin loader component start"); }
+plugin *plugin::_instance = nullptr;
+std::mutex plugin::_mutex;
 
-plugin::~plugin() { logs("Plugin loader component stop"); }
-
-const std::pair<bool, const std::string> plugin::find(const std::pair<const std::string, const std::string> &plugin)
+plugin *plugin::get(void)
 {
-    const std::string path(plugin.first.empty() ? (_plugin_dir + '/' + plugin.second) : (_plugin_dir + '/' + plugin.first + '/' + plugin.second));
+    std::lock_guard<std::mutex> lock(_mutex);
+    _instance == nullptr && (_instance = new plugin{});
+    return _instance;
+}
+
+const std::pair<bool, const std::string> plugin::find(const std::string &plugin)
+{
+    const std::string path(plugin);
     if (std::filesystem::status(path).type() != std::filesystem::file_type::not_found)
         return {true, std::string(path)};
     return {false, ""};
 }
 
-void plugin::load(void) {}
+void plugin::list(void)
+{
+    std::size_t at = 1;
+    for (std::filesystem::recursive_directory_iterator dir("../Module/"), end; dir != end; dir++)
+        if (!std::filesystem::is_directory(dir->path()) && std::filesystem::path(dir->path()).extension() == ".so") {
+            _list.push_back(dir->path().c_str());
+            std::cout << '\t' << at << " " << dir->path().c_str() << std::endl;
+            at++;
+        }
+}
 
-void plugin::remove(void) {}
+void plugin::list(const std::string &files)
+{
+    std::size_t at = 1;
+    for (std::filesystem::recursive_directory_iterator dir(files), end; dir != end; dir++)
+        if (!std::filesystem::is_directory(dir->path())) {
+            _list.push_back(dir->path().c_str());
+            std::cout << '\t' << at << " " << dir->path().c_str() << std::endl;
+            at++;
+        }
+}
+
+void plugin::load(const std::string &plugin)
+{
+    const auto &load = find(plugin);
+    if (load.first && load.second.substr(10, 4) == "Game")
+        _game == nullptr ? (_game = _game_lib.load_unique<interface::logic::game>(plugin, "entry_point"))
+        : (remove("Game"), _game = _game_lib.load_unique<interface::logic::game>(plugin, "entry_point"));
+    else if (load.first && load.second.substr(10, 7) == "Graphic")
+        if (load.second.substr(18, 2) == "2D") {
+            (_graphic_2D == nullptr || _graphic_3D == nullptr) ? (_graphic_2D = _graphic_lib.load_unique<interface::graphic::lib2D>(plugin, "entry_point"))
+            : (remove("Graphic"), _graphic_2D = _graphic_lib.load_unique<interface::graphic::lib2D>(plugin, "entry_point"));
+        } else if (load.second.substr(18, 2) == "3D") {
+            (_graphic_3D == nullptr || _graphic_2D == nullptr) ? (_graphic_3D = _graphic_lib.load_unique<interface::graphic::lib3D>(plugin, "entry_point"))
+            : (remove("Graphic"), _graphic_3D = _graphic_lib.load_unique<interface::graphic::lib3D>(plugin, "entry_point"));
+        }
+}
+
+void plugin::remove(const std::string &plugin)
+{
+    if (plugin == "Game") {
+        _game->~game();
+        _game = nullptr;
+        _game_lib.free();
+    } else if (plugin == "Graphic") {
+        _graphic_2D->~lib2D();
+        _graphic_3D->~lib3D();
+        _graphic_2D = nullptr;
+        _graphic_3D = nullptr;
+        _graphic_lib.free();
+    }
+}
+
+void plugin::clear(void)
+{
+    if (_game != nullptr) {
+        _game = nullptr;
+        _game_lib.free();
+    }
+    if (_graphic_2D != nullptr) {
+        _graphic_2D = nullptr;
+        _graphic_lib.free();
+    }
+    if (_graphic_3D != nullptr) {
+        _graphic_3D = nullptr;
+        _graphic_lib.free();
+    }
+}
--- Core/Components/Plugin Loader/plugin.hpp
+++ Core/Components/Plugin Loader/plugin.hpp
@@ -1,9 +1,11 @@
-#ifndef PLUGIN_LOADER
-#define PLUGIN_LOADER
+#ifndef PLUGIN
+#define PLUGIN
 
 /* Source */
 #include "ressource.hpp"
 #include "dynamic_library_loader.hpp"
+#include "logic.hpp"
+#include "graphic.hpp"
 
 namespace nexus {
 
@@ -13,17 +15,37 @@ namespace nexus {
 
             class plugin {
                 public:
-                    plugin();
-                    ~plugin();
+                    void operator=(const plugin &) = delete;
+                    plugin(const plugin &) = delete;
 
-                    const std::pair<bool, const std::string> find(const std::pair<const std::string, const std::string> &plugin);
+                    static plugin *get(void);
+                    void destroy(void) const { delete get(); }
 
-                    void load(void);
-                    void remove(void);
-                
+                    const std::string get_list_at(const std::size_t position) const { if (position < _list.size() && position >= 0) return _list.at(position); return ""; }
+
+                    const std::pair<bool, const std::string> find(const std::string &plugin);
+                    void list(void);
+                    void list(const std::string &files);
+                    void load(const std::string &plugin);
+                    void remove(const std::string &plugin);
+                    void clear(void);
+
                 protected:
+                    plugin() { logs("Plugin loader component start"); }
+                    ~plugin()
+                    {
+                        clear();
+                        logs("Plugin loader component stop");
+                    }
                 private:
-                    std::string _plugin_dir = "../../Plugin";
+                    static plugin *_instance;
+                    static std::mutex _mutex;
+                    std::vector<std::string> _list;
+                    dynamic_library_loader _game_lib;
+                    dynamic_library_loader _graphic_lib;
+                    std::unique_ptr<interface::logic::game> _game = nullptr;
+                    std::unique_ptr<interface::graphic::lib2D> _graphic_2D = nullptr;
+                    std::unique_ptr<interface::graphic::lib3D> _graphic_3D = nullptr;
             };
 
         } // components
@@ -33,4 +55,4 @@ namespace nexus {
 } // nexus
 
 
-#endif /* PLUGIN_LOADER */
+#endif /* PLUGIN */
