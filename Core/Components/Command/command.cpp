e0c27466b9c5a26cfd52975f0c735d3eeb75eeb2
nichevoya <isaak.nicolas@epitech.eu>
2023-04-29 08:35:11 +0200

    [+] doc list command, details doc load command

--- Core/Components/Command/command.cpp
+++ Core/Components/Command/command.cpp
@@ -10,7 +10,8 @@ void command::help(void) const
 {
     logs("\thelp - display helps");
     logs("\tq, quit - global shutdown");
-    logs("\tload - load open a subsequent environment from which you can load libraries into memory, it then returns to this environment");
+    logs("\tload - load open a subsequent environment from which you can load libraries into memory, it then returns to this environment, memory is already handled so you can use this command to load another library, it will remove the previous one along the way");
+    logs("\tlist - list open a subsequent environment from which you can recursively list files into your console, it always starts from nexus binary location");
 }
 
 void command::quit(void) const { _app->set_status(false); }
