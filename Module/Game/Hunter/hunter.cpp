d8fa00bbbc5be0de873cb913610df31dc677d804
nichevoya <isaak.nicolas@epitech.eu>
2023-07-25 15:04:28 +0200

    [+] hunter library : ecs implementation

--- Module/Game/Hunter/hunter.cpp
+++ Module/Game/Hunter/hunter.cpp
@@ -4,13 +4,45 @@ using namespace nexus::module::logic;
 
 extern "C" const std::unique_ptr<hunter> entry_point(void) { return std::make_unique<hunter>(); }
 
-hunter::hunter() { logs("Hunter start"); }
+hunter::hunter()
+{
+    logs("Hunter start");
+    _engine.initialization();
+    this->initialization();
+}
 
 hunter::~hunter() { logs("Hunter stop"); }
 
+
+void hunter::initialization(void)
+{
+    factory();
+    assemble();
+}
+
+void hunter::factory(void)
+{
+    component_factory();
+    system_factory();
+}
+
+void hunter::assemble(void)
+{
+    assemble_targets();
+}
+
+const std::map<std::string, std::vector<std::string>> &hunter::get_texture_file_list(void) const { return _texture_file_list; }
+
 void hunter::update(void)
 {
-    
+    systems_update();
+}
+
+void hunter::systems_update(void)
+{
+    for (const auto &system : _systems) {
+        system->update(_engine, _entities);
+    }
 }
 
 bool hunter::status(void) const { return _status; }
--- Module/Game/Hunter/hunter.hpp
+++ Module/Game/Hunter/hunter.hpp
@@ -5,25 +5,87 @@
 #include "ressource.hpp"
 #include "logic.hpp"
 
+/* -------------------------------- Construct ------------------------------- */
+#include "construct.hpp"
+
+/* --------------------------------- Engine --------------------------------- */
+#include "coordinator.hpp"
+
+/* -------------------------------- Interface ------------------------------- */
+#include "abstract.hpp"
+
+/* -------------------------------- Component ------------------------------- */
+#include "force.hpp"
+#include "position.hpp"
+#include "transform.hpp"
+
+/* --------------------------------- System --------------------------------- */
+#include "movable.hpp"
+
 namespace nexus {
 
     namespace module {
 
         namespace logic {
 
-            using namespace nexus::core::interface::logic;
-
-            class hunter : public game {
+            class hunter : public nexus::core::interface::logic::game {
                 public:
                     hunter();
                     ~hunter();
 
+                    void initialization(void);
+                    void factory(void);
+                        void component_factory(void)
+                        {
+                            //NOTE - Space
+                            _engine.register_component<factory::components::force>();
+                            _engine.register_component<factory::components::position>();
+                            _engine.register_component<factory::components::transform>();
+                            
+                        }
+
+                        void system_factory(void)
+                        {
+                            auto movable = _engine.register_system<factory::system::movable>();
+                            {
+                                _engine.set_system_signature<factory::system::movable>(engine::signature{}.set(_engine.get_component_type<factory::components::transform>()));
+                            }
+
+                            _systems.push_back(movable);
+                        }
+
+                    void assemble(void);
+                        void assemble_targets(void)
+                        {
+                            for(std::size_t nb = 0; nb < 1; nb++) {
+                                const auto entity = _engine.create_entity();
+                                _engine.add_component<factory::components::transform>(entity, factory::components::transform{});
+                                _targets.push_back(entity);
+                            }
+
+                            _entities.insert({factory::entity::type::target, _targets});
+                        }
+
+                    const std::map<std::string, std::vector<std::string>> &get_texture_file_list(void) const override;
+
                     void update() override;
+                        void systems_update(void);
+
                     bool status(void) const override;
 
                 protected:
                 private:
                     bool _status = true;
+
+                    engine::coordinator _engine;
+                    factory::entity::construct _construct;
+
+                    std::map<std::string, std::vector<std::string>> _texture_file_list;
+                    
+                    std::vector<std::shared_ptr<factory::abstract::system>> _systems;
+
+                    std::vector<engine::entity> _targets;
+                    std::map<const factory::entity::type, std::vector<engine::entity> &> _entities;                    
             };
             
         } // logic
