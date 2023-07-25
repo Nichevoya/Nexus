7bc7a7edd3d7a971e02b917290536bb3504936f7
nichevoya <isaak.nicolas@epitech.eu>
2023-07-25 16:04:20 +0200

    [+] random utility object, library comments

--- Core/App/ressource.hpp
+++ Core/App/ressource.hpp
@@ -14,10 +14,13 @@
     #define err_logs(message)
 #endif
 
+#define screen_width 1920
+#define screen_height 1080
+
 /* ---------------------------- Standard Library ---------------------------- */
 #include <iostream>
-#include <string>
 #include <assert.h>
+#include <string>
 
 #include <filesystem>
 
@@ -30,6 +33,7 @@
 #include <mutex>
 
 #include <chrono>
+#include <random>
 
 #include <unordered_map>
 #include <vector>
@@ -39,29 +43,85 @@
 
 
 /* -------------------------------- Exception ------------------------------- */
+
+/**
+ * @brief Exception handler object
+ * 
+ */
 class exception : public std::runtime_error {
     public: exception(const std::string &message) : std::runtime_error(message) { err_logs("Exception:"); }
 };
 
-/* ------------------------------ Global ------------------------------ */
-
-/* -------------------------------- App ------------------------------- */
 namespace nexus {
 
     namespace core {
 
+        /* ------------------------------ Global ------------------------------ */
+        namespace global {
+
+            /**
+             * @brief Global random utility object
+             * 
+             */
+            class random {
+                public:
+
+                    /**
+                     * @brief Return a random integer between 0 and RAND_MAX inclusive
+                     * 
+                     * @return int 
+                     */
+                    static int number(void) { return std::rand(); }
+                    
+                    /**
+                     * @brief Return a random integer between the given range inclusive
+                     * 
+                     * @param from Lowest number from which the range will start
+                     * @param to Highest number from which the range will stop
+                     * @return int 
+                     */
+                    static int ranged_number(const int from, const int to) { return std::rand() % (to + 1 - from) + from; }
+            
+                protected:
+                private:
+            };
+            
+        } // global
+        
+        /* -------------------------------- App ------------------------------- */
         namespace app {
 
             namespace integrity {
 
+                /**
+                 * @brief Define the application status and gives control over it
+                 * 
+                 */
                 class status {
                     public:
                         void operator=(const status &) = delete;
                         status(const status &) = delete;
 
+                        /**
+                         * @brief Return a unique reference to the status object
+                         * 
+                         * @return status& 
+                         */
                         static status &get(void) { static status instance; return instance; };
 
+                        /**
+                         * @brief Set the status
+                         * 
+                         * @param status 
+                         */
                         void set_status(const bool status) { get()._status = status; }
+                        
+                        /**
+                         * @brief Get the status
+                         * 
+                         * @return true 
+                         * @return false 
+                         */
                         bool get_status(void) const { return get()._status; }
 
                     protected:
